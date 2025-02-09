a026e48dc606823e09893a54d3c5b49b4f8d01f8
akihiko.kigure <akihiko.kigure@gmail.com>
2025-02-09 15:02:22 +0900

    feat: add wirte port config, update senddata

--- apps/dashboard/src/app/service/web-serial/web-serial.service.ts
+++ apps/dashboard/src/app/service/web-serial/web-serial.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
-import { catchError, from, map, Observable, throwError } from 'rxjs';
+import { catchError, finalize, from, map, Observable, throwError } from 'rxjs';
 import {
   WEB_SERIAL_ERROR_PORT_ALERADY_OPEN,
   WEB_SERIAL_ERROR_PORT_NO_SELECTED,
   WEB_SERIAL_ERROR_PORT_OPEN_FAIL,
+  WEB_SERIAL_ERROR_PORT_WRITABLE_FAIL,
   WEB_SERIAL_ERROR_UNKNOWN,
   WEB_SERIAL_IS_NOT_RASPBEYY_PI_ZERO,
   WEB_SERIAL_OPEN_SUCCESS,
+  WEB_SERIAL_PORT_WRITABLE_SUCCESS,
 } from '../../constants';
 import { isRaspberryPiZero } from '../../functions';
 
@@ -15,6 +17,8 @@ import { isRaspberryPiZero } from '../../functions';
 })
 export class WebSerialService {
   private port: SerialPort | null;
+  private writer: WritableStreamDefaultWriter | null = null;
+  private encoder = new TextEncoderStream();
 
   constructor() {
     this.port = null;
@@ -28,12 +32,27 @@ export class WebSerialService {
       const isPiZero = await isRaspberryPiZero(this.port);
 
       if (isPiZero) {
+        this.configPortWriter();
         return WEB_SERIAL_OPEN_SUCCESS;
       } else {
         return WEB_SERIAL_IS_NOT_RASPBEYY_PI_ZERO;
       }
     } catch (error) {
       return this.connectError(error);
+    }
+  }
+
+  configPortWriter() {
+    try {
+      if (this.port == null || this.port.writable == null) {
+        return WEB_SERIAL_ERROR_PORT_WRITABLE_FAIL;
+      } else {
+        this.writer = this.encoder.writable.getWriter();
+        this.encoder.readable.pipeTo(this.port.writable);
+        return WEB_SERIAL_PORT_WRITABLE_SUCCESS;
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
@@ -56,6 +75,9 @@ export class WebSerialService {
 
   async disConnect() {
     try {
+      if (this.writer) {
+        await this.writer.releaseLock();
+      }
       await this.port?.close();
     } catch (error) {}
   }
@@ -65,19 +87,17 @@ export class WebSerialService {
       return throwError(() => new Error('Serial port not connected'));
     }
 
-    const encoder = new TextEncoder();
-    const writer = this.port.writable?.getWriter();
-    if (!writer) {
+    if (!this.writer) {
       return throwError(() => new Error('Serial port not connected'));
     }
 
-    return from(writer.write(encoder.encode(data))).pipe(
+    return from(this.writer?.write(data)).pipe(
+      finalize(() => this.writer?.close()),
       map(() => {
-        writer.releaseLock();
+        // todo
       }),
       catchError((error) => {
         console.error('Error sending data:', error);
-        writer.releaseLock();
         return throwError(() => error);
       }),
     );
