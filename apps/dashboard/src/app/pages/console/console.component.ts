5d1140c65c0226b24770d3d58d5516926e71f7b4
akihiko.kigure <akihiko.kigure@gmail.com>
2025-02-24 16:49:37 +0900

    refactor: remove console container

--- apps/dashboard/src/app/pages/console/console.component.ts
+++ apps/dashboard/src/app/pages/console/console.component.ts
@@ -1,9 +1,77 @@
-import { Component } from '@angular/core';
-import { ConsoleContainersComponent } from '../../containers';
+import { Dialog } from '@angular/cdk/dialog';
+import { AfterViewInit, Component, inject } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { Store } from '@ngrx/store';
+import { Terminal } from '@xterm/xterm';
+import { ConsoleToolBarComponent } from '../../components';
+import { xtermConsoleConfigOptions } from '../../models';
+import { WebSerialService, XtermService } from '../../service';
+import { DialogService } from '../../service/dialog/dialog.service';
 
 @Component({
   selector: 'choh-console',
-  imports: [ConsoleContainersComponent],
-  template: `<choh-console-containers />`,
+  imports: [ConsoleToolBarComponent, MatDividerModule],
+  template: `
+    <choh-console-tool-bar
+      (eventWiFiSetting)="openWifiSettingDialog()"
+      (eventCreateFile)="createFile()"
+      (eventGetExample)="openExampleFrameDialog()"
+      (eventSetupChirimen)="openSetupChirimenDialog()"
+      (eventI2CDetect)="openI2CDetectDialog()"
+      (eventFileUpload)="openFileUploadDialog()"
+    />
+    <div id="consoleDom"></div>
+  `,
+  styleUrl: './console.component.scss',
+  providers: [WebSerialService],
 })
-export default class ConsoleComponent {}
+export default class ConsoleComponent implements AfterViewInit {
+  store = inject(Store);
+  service = inject(WebSerialService);
+  xtermService = inject(XtermService);
+  dialogService = inject(DialogService);
+  dialog = inject(Dialog);
+
+  label = 'connect';
+  xterminal = new Terminal(xtermConsoleConfigOptions);
+  consoleDom: HTMLElement | null = null;
+
+  ngAfterViewInit(): void {
+    this.configTerminal();
+  }
+
+  openWifiSettingDialog() {
+    this.dialogService.openWifiSettingDialog();
+  }
+
+  createFile() {}
+
+  openExampleFrameDialog() {
+    this.dialogService.openExampleFrameDialog();
+  }
+
+  openSetupChirimenDialog() {
+    this.dialogService.openSetupChirimenDialog();
+  }
+  openI2CDetectDialog() {
+    this.dialogService.openI2CDetectDialog();
+  }
+
+  openFileUploadDialog() {
+    this.dialogService.openFileUploadDialog();
+  }
+
+  private configTerminal() {
+    this.consoleDom = document.getElementById('consoleDom');
+    if (this.consoleDom) {
+      this.xterminal.open(this.consoleDom);
+    } else {
+      return;
+    }
+
+    this.xterminal.reset();
+    this.xterminal.writeln('$ ');
+
+    this.xterminal.onKey((e) => this.xtermService.onKey(this.xterminal, e));
+  }
+}
