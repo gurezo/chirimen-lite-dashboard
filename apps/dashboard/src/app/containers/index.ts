9f78a10cdfdce1d85d47c7730b4d31fb87cae28a
akihiko.kigure <akihiko.kigure@gmail.com>
2025-02-24 16:49:56 +0900

    refactor: remove container folder

--- apps/dashboard/src/app/containers/index.ts
+++
@@ -1,5 +0,0 @@
-import { ConsoleContainersComponent } from './console-containers/console-containers.component';
-
-const containers = [ConsoleContainersComponent];
-
-export { ConsoleContainersComponent, containers };
