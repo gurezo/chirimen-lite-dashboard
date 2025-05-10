2715745f27fee3140ce8422754f59c14eb8b509a
akihiko.kigure <akihiko.kigure@gmail.com>
2025-05-10 12:38:55 +0900

    refactor: replace tailwindcss for page-not-found

--- apps/dashboard/src/app/pages/page-not-found/page-not-found.component.ts
+++ apps/dashboard/src/app/pages/page-not-found/page-not-found.component.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
   selector: 'choh-page-not-found',
   imports: [],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss',
 })
 export default class PageNotFoundComponent {}
