16fccf870442f70dc37f93472330abd8d486acd9
akihiko.kigure <akihiko.kigure@gmail.com>
2025-05-18 17:15:53 +0900

    refactor: remove scss for app

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { IconService } from './shared/service';
   selector: 'choh-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'CHIRIMEN Lite DashBoard';
