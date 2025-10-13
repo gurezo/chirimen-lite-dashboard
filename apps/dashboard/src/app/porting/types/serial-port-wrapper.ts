5bb60c6f51fb94cb27022d032ace76383d8295e7
akihiko.kigure <akihiko.kigure@gmail.com>
2025-10-13 16:39:19 +0900

    refactor: remove port code

--- apps/dashboard/src/app/porting/types/serial-port-wrapper.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@types/w3c-web-serial" />
-
-export interface SerialPortWrapper {
-  port: SerialPort;
-  reader: ReadableStreamDefaultReader<Uint8Array>;
-  writer: WritableStreamDefaultWriter<Uint8Array>;
-}
