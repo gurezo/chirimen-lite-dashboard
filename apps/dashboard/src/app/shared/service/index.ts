4661267aa66daa0a6b797d6be90ef2731a0cb37d
akihiko.kigure <akihiko.kigure@gmail.com>
2025-10-13 14:20:35 +0900

    refactor: remove no use code

--- apps/dashboard/src/app/shared/service/index.ts
+++ apps/dashboard/src/app/shared/service/index.ts
@@ -7,4 +7,3 @@ export * from './notification/serial-notification.service';
 export * from './serial';
 export * from './terminal/terminal.service';
 export * from './toast-message/toast-message.service'; // 後方互換性のため残す（deprecated）
-export * from './xterm/xterm.service';
--- apps/dashboard/src/app/shared/web-serial/service/index.ts
+++ apps/dashboard/src/app/shared/web-serial/service/index.ts
@@ -1,3 +1,1 @@
-export * from './web-serial.reader';
 export * from './web-serial.service';
-export * from './web-serial.writer';
