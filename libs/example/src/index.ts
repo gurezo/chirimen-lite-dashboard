99e85096411184fc65cbdb8eb0a27eb5c3b4fa1f
akihiko.kigure <akihiko.kigure@gmail.com>
2025-10-14 22:02:20 +0900

    refactor: split example

---
+++ libs/example/src/index.ts
@@ -0,0 +1,15 @@
+// Components
+export * from './lib/components/example-item/example-item.component';
+export * from './lib/components/example-list/example-list.component';
+export * from './lib/example.component';
+
+// Services
+export * from './lib/services/example.dialog.service';
+export * from './lib/services/example.service';
+
+// Models
+export * from './lib/models/example.item.model';
+export * from './lib/models/example.model';
+
+// Functions
+export * from './lib/functions/convert';
