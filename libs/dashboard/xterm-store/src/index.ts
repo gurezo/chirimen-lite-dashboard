1bd12c65ca77261231ab6bc636148b73f9b89cf5
akihiko.kigure <akihiko.kigure@gmail.com>
2025-10-26 16:29:32 +0900

    feat: implement @dashboard/xterm-store library and fix app.config.ts imports

--- libs/dashboard/xterm-store/src/index.ts
+++ libs/dashboard/xterm-store/src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/xterm-store.module';
+export * from './lib/xterm.actions';
+export * from './lib/xterm.effects';
+export * from './lib/xterm.model';
+export * from './lib/xterm.reducers';
+export * from './lib/xterm.selectors';
---
+++ libs/dashboard/xterm-store/src/lib/xterm.selectors.ts
@@ -0,0 +1,24 @@
+import { createSelector } from '@ngrx/store';
+import { XtermState } from './xterm.model';
+
+export const xtermFeatureKey = 'xterm';
+
+export const selectXTermFeature = (state: { xterm: XtermState }) => state.xterm;
+
+export const selectInput = createSelector(
+  selectXTermFeature,
+  (state) => state.input
+);
+export const selectOutput = createSelector(
+  selectXTermFeature,
+  (state) => state.output
+);
+export const selectConnectionStatus = createSelector(
+  selectXTermFeature,
+  (state) => state.connected
+);
+export const selectError = createSelector(
+  selectXTermFeature,
+  (state) => state.error
+);
+
