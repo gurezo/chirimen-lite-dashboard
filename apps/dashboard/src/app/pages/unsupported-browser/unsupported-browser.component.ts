e2ee2c67c2847427f24503f0d6748f129c93a8a3
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-03 18:56:54 +0900

    refactor: add defer and loading

--- apps/dashboard/src/app/pages/unsupported-browser/unsupported-browser.component.ts
+++ apps/dashboard/src/app/pages/unsupported-browser/unsupported-browser.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'choh-unsupported-browser',
-  imports: [],
+  imports: [MatProgressSpinner],
   templateUrl: './unsupported-browser.component.html',
+  styles: [
+    `
+      .fade-in {
+        animation: fadeIn 0.3s ease-in;
+      }
+
+      @keyframes fadeIn {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 1;
+        }
+      }
+    `,
+  ],
 })
 export default class UnsupportedBrowserComponent {}
