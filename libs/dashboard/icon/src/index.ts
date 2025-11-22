a993dfcd07393918f35d7fd1d57c39fdfcb31ad9
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-22 15:31:28 +0900

    refactor(libs-dashboard-icon): remove libs-dashboard-icon

--- libs/dashboard/icon/src/index.ts
+++
@@ -1,5 +0,0 @@
-// IconService has been moved to @libs-shared
-// All icon functionality is now available through @libs-shared
-
-// Dummy export to prevent build errors
-export const DEPRECATED_ICON_LIBRARY = 'This library is deprecated';
