8d11c8e31646ba82d1955004f025c4dc0da0b0a4
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-22 15:32:20 +0900

    refactor(libs-dashboard-xterm-store): remove libs-dashboard-xterm-store

--- libs/dashboard/xterm-store/src/index.ts
+++
@@ -1,5 +0,0 @@
-// All xterm store functionality has been moved to @libs-xterm
-// This library is now deprecated
-
-// Dummy export to prevent build errors
-export const DEPRECATED_XTERM_STORE_LIBRARY = 'This library is deprecated';
