47961ff0982e30b21a53692e464db7c1e76b4014
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-22 15:30:38 +0900

    refactor(libs-dashboard-chirimen): remove libs-dashboard-chirimen

--- libs/dashboard/chirimen/src/index.ts
+++
@@ -1,5 +0,0 @@
-// ChirimenService has been moved to @libs-chirimen-panel
-// All CHIRIMEN functionality is now available through @libs-chirimen-panel
-
-// Dummy export to prevent build errors
-export const DEPRECATED_CHIRIMEN_LIBRARY = 'This library is deprecated';
