662c64903b7fbcfa0218d53b6f15dbf17b7c0be7
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-22 15:31:52 +0900

    refactor(libs-dashboard-notification): remove libs-dashboard-notification

--- libs/dashboard/notification/src/index.ts
+++
@@ -1,6 +0,0 @@
-// All notification services have been moved to appropriate libraries
-// - NotificationService -> @libs-shared
-// - SerialNotificationService -> @libs-web-serial
-
-// Dummy export to prevent build errors
-export const DEPRECATED_NOTIFICATION_LIBRARY = 'This library is deprecated';
