3eb10fccf45a5cf2616c6930fe01592839ca0413
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-22 15:31:05 +0900

    refactor(libs-dashboard-editor): remove libs-dashboard-editor

--- libs/dashboard/editor/src/index.ts
+++
@@ -1,5 +0,0 @@
-// EditorService has been moved to @libs-editor
-// All editor functionality is now available through @libs-editor
-
-// Dummy export to prevent build errors
-export const DEPRECATED_EDITOR_LIBRARY = 'This library is deprecated';
