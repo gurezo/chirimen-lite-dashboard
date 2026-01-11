07e4308b1f7097f324d936a6cac28b763134fce1
akihiko.kigure <akihiko.kigure@gmail.com>
2026-01-11 16:33:57 +0900

    feat(libs): add page-not-found library
    - Generate page-not-found library using Nx
    - Configure build, test, and lint targets

---
+++ libs/page-not-found/src/lib/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'choh-page-not-found',
+  imports: [],
+  templateUrl: './page-not-found.component.html',
+})
+export default class PageNotFoundComponent {}
