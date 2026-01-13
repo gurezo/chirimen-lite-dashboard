d730a8479444659a6fba7993738acc0a346aec4f
akihiko.kigure <akihiko.kigure@gmail.com>
2026-01-13 22:28:22 +0900

    refactor: remove create file

--- apps/dashboard/src/app/dialog/index.ts
+++
@@ -1,6 +0,0 @@
-import { FileUploadComponent } from '@libs-file-upload';
-import { CreateFileComponent } from './create-file/create-file.component';
-
-const dialogs = [CreateFileComponent, FileUploadComponent];
-
-export { CreateFileComponent, dialogs, FileUploadComponent };
