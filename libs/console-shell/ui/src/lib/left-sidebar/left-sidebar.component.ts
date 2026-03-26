5c9493de89ec86044a37e168c56a7ca3c38c7709
akihiko.kigure <akihiko.kigure@gmail.com>
2026-03-26 21:18:29 +0900

    feat(file-manager): implement tree UI and mount in left sidebar
    Render directory/file entries in file-tree component, wire navigation in file-tree-feature, and display it inside the console left sidebar.

--- libs/console-shell/ui/src/lib/left-sidebar/left-sidebar.component.ts
+++ libs/console-shell/ui/src/lib/left-sidebar/left-sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FileTreeFeatureComponent } from '@libs-file-manager-feature';
 
 @Component({
   selector: 'lib-left-sidebar',
   standalone: true,
+  imports: [FileTreeFeatureComponent],
   template: `
-    <div class="left-sidebar">
-      <p>Left sidebar (stub)</p>
+    <div class="left-sidebar h-full border-r border-gray-200">
+      <lib-file-tree-feature />
     </div>
   `,
 })
