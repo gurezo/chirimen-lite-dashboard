bfd602e78c5f98c4ef7c108cca8e48f7b7898a13
akihiko.kigure <akihiko.kigure@gmail.com>
2026-03-28 18:58:05 +0900

    feat(console-shell): add breadcrumb component

--- libs/console-shell/ui/src/index.ts
+++ libs/console-shell/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/breadcrumb/breadcrumb.component';
 export * from './lib/header-toolbar/header-toolbar.component';
 export type { ToolbarAction } from './lib/header-toolbar/header-toolbar.component';
 export * from './lib/left-sidebar/left-sidebar.component';
